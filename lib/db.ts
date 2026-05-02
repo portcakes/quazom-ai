60bcd7795d61e7427a4d106a1e532b5a7c866f49
Cayla Jefferson <caylajefferson800@gmail.com>
2026-05-02 15:50:03 -0400

    Add initial project setup with Prisma, UI components, and Tailwind CSS integration

---
+++ lib/db.ts
@@ -0,0 +1,21 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "../lib/generated/prisma/client";
+import "dotenv/config";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+function createPrismaClient() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+  }
+  return new PrismaClient({
+    adapter: new PrismaNeon({ connectionString }),
+  });
+}
+
+const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
